da658acd376dde0b750804b0f442c5556ff6e0de
pn <pondzik0@gmail.com>
2021-03-25 20:24:56 +0100

    zmieniono:       contracts/MasterChef.sol 	zmieniono:       contracts/Migrations.sol 	zmieniono:       contracts/Timelock.sol 	zmieniono:       contracts/libs/BEP20.sol 	zmieniono:       contracts/libs/SafeBEP20.sol 	zmieniono:       package.json 	zmieniono:       yarn.lock

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+
 
 // COPIED FROM https://github.com/compound-finance/compound-protocol/blob/master/contracts/Governance/GovernorAlpha.sol
 // Copyright 2020 Compound Labs, Inc.
